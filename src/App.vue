74b2e3f6416cd037baab89d58c02180737c89dee
duttdutt <ynksdk1654@gmail.com>
2024-09-27 15:32:08 +0300

    feature: created footer component with clock

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-    import HeaderLayout from './components/HeaderLayout.vue';
-    import BodyLayout from './components/BodyLayout.vue';
-    import FooterLayout from './components/FooterLayout.vue';
+import HeaderLayout from "./components/HeaderLayout.vue"
+import BodyLayout from "./components/BodyLayout.vue"
+import FooterLayout from "./components/FooterLayout.vue"
 </script>
 
 <template>
-    <HeaderLayout />
-    <BodyLayout />
-    <FooterLayout />
+  <HeaderLayout />
+  <BodyLayout />
+  <FooterLayout />
 </template>
 
 <style scoped></style>
--- src/components/FooterLayout.vue
+++ src/components/FooterLayout.vue
@@ -1,9 +1,40 @@
 <script setup lang="ts">
+import { onMounted, ref, computed } from "vue"
+import { getMonth } from "../help/getMonth"
 
+const now = ref<Date>(new Date())
+
+const updateTime = (): void => {
+  now.value = new Date()
+}
+
+onMounted(() => {
+  setInterval(updateTime, 1000)
+})
+
+const date = computed<number>(() => now.value.getDate())
+const month = computed<string>(() => getMonth(now.value.getMonth()))
+const hours = computed<number>(() => now.value.getHours())
+const minutes = computed<string>(() =>
+  now.value.getMinutes() < 10
+    ? "0" + now.value.getMinutes()
+    : String(now.value.getMinutes()),
+)
 </script>
 
 <template>
-    <div>Footer</div>
+  <footer>
+    {{ `${date} ${month}, ${hours}:${minutes}` }}
+  </footer>
 </template>
 
-<style scoped></style>
+<style scoped>
+footer {
+  padding-top: 18px;
+  height: 7vh;
+  text-align: center;
+  user-select: none;
+  background-color: #f6f6f8;
+  font-weight: bold;
+}
+</style>
