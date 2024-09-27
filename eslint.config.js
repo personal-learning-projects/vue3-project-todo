e8505f7454e8820325457905b68c41596a8cf167
duttdutt <ynksdk1654@gmail.com>
2024-09-27 03:56:39 +0300

    chore: fixed some settings

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,6 @@ import pluginVue from "eslint-plugin-vue"
 
 export default [
     ...pluginVue.configs["flat/recommended"],
-    ...pluginVue.configs["flat/strongly-recommended"],
     {
         rules: {
             "vue/html-indent": "off",
