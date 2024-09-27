0181bc96cc44fc1b201d73d9f0763d119d4904b4
duttdutt <ynksdk1654@gmail.com>
2024-09-27 15:31:06 +0300

    util: added helper function

---
+++ src/help/getMonth.ts
@@ -0,0 +1,22 @@
+export function getMonth(monthNumber: number) {
+  switch (monthNumber) {
+    case 0:
+      return "January"
+    case 1:
+      return "February"
+    case 2:
+      return "March"
+    case 3:
+      return "April"
+    case 4:
+      return "May"
+    case 5:
+      return "June"
+    case 6:
+      return "July"
+    case 7:
+      return "August"
+    case 8:
+      return "September"
+  }
+}
